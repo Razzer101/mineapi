ca25b09da9636ba8ea3c49b5821a02ea786fe4a4
Razzer101 <Rasmus@anders-net.dk>
2026-02-27 12:43:47 +0100

    Feat: Added more examples to index

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $obj = new stdClass;
 
 $obj->all_mobs = "http://$_SERVER[SERVER_NAME]/mineapi/mobs.php";
 $obj->one_mob_example = "http://$_SERVER[SERVER_NAME]/mineapi/mobs.php?id=1";
+$obj->offset_example = "http://$_SERVER[SERVER_NAME]/mineapi/mobs.php?offset=20";
+$obj->limit_example = "http://$_SERVER[SERVER_NAME]/mineapi/mobs.php?limit=50";
+$obj->offset_and_limit_example = "http://$_SERVER[SERVER_NAME]/mineapi/mobs.php?offset=10&limit=20";
 
 header("Content-Type:application/json");
 echo json_encode($obj);
